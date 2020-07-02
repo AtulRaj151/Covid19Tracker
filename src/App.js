a3b229766e254432b0ee742e9e813532b1083776
unknown <atulsid151@gmail.com>
2020-07-02 11:50:57 +0530

    add folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { Cards,Chart,CountryPicker} from './Component';
 class App extends React.Component {
      
     render() {
           return(
 
                <div>
-                   <h1>App</h1>
+                   <Cards/>
+                   <CountryPicker/>
+                   <Chart/>
                </div>
           )
     }
