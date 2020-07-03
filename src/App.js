5f6c0232304a67bcc718f6308dfdb0b213c32fe4
unknown <atulsid151@gmail.com>
2020-07-03 11:25:24 +0530

    add for production

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Cards,Chart,CountryPicker} from './Component';
 import styles from './App.module.css';
 import {fetchData} from './api/index';
+
+import  coronaImage from './images/image.png'
 class App extends React.Component {
 
     //constructor automatically callled
@@ -31,16 +33,16 @@ class App extends React.Component {
     }
      
     render() {
-        const {data} = this.state;
+        const {data, country} = this.state;
 
           return(
 
                <div className={styles.container}>
-
+                     <img className={styles.image} src={coronaImage} alt="COVID-19" />
                    {/* pass data to the card as props */}
                    <Cards  data={data}/>
                    <CountryPicker handleCountryChange={this.handleCountryChange} />
-                   <Chart/>
+                   <Chart data={data} country={country}/>
                </div>
           )
     }
